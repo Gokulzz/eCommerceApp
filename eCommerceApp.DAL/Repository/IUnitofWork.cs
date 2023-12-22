480cf2c56bce2e2af0cea45ccb68c32c643bd6e8
Gokulzz <gokulkhatri2020@gmail.com>
2023-12-22 00:28:54 -0500

    creating a unit of work pattern which contains all our repository then injecting datacontext object to each specific repository.We will use this reposiotry to communicate with specific table in our database

---
+++ eCommerceApp.DAL/Repository/IUnitofWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.DAL.Repository
+{
+    public interface IUnitofWork
+    {
+        public IUserRepository UserRepository { get; }
+        public IRoleRepository RoleRepository { get; }
+        public IProductRepository ProductRepository { get; }
+        public IOrderRepository OrderRepository { get; }
+        public IOrderdetailRepository OrderdetailRepository { get; }
+        public ICartRepository CartRepository { get; }
+        public ICartItemRepository CartItemRepository { get; }
+        public IPaymentRepository PaymentRepository { get; }
+        public IPaymentMethodRepository PaymentMethodRepository { get; }
+        public ICategoryRepository categoryRepository { get; }
+        public Task Save();
+    }
+}
