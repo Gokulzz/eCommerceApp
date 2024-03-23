6582fa924a9889591954ee61bd42d401bed01aa1
Gokulzz <gokulkhatri2020@gmail.com>
2024-03-23 00:36:23 -0400

    adding new pages

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@
 
 import express from 'express';
 
+
 const app = express();
 const port = 3000;
 
@@ -9,19 +10,14 @@ const port = 3000;
 app.use('/public', express.static('public'));
 
 // Define a simple array of products
-const products = [
-  { id: 1, name: 'iPhone 15 pro max', price: 1119.99, image: 'iphone.jpg' },
-  { id: 2, name: 'Samsung galaxy s22 Ultra', price: 1029.99, image: 'SAMSUNG.jpg' },
-  {id:3, name:"Electric trimmer", price:50, image:'razor.jpg'}
-  // Add more products as needed
-];
+
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
-  res.render('pages/home', { title: "Home Page", products: products });
+  res.render('pages/home', { title: "Home Page"});
 });
 app.get('/account', (req, res)=> {
   res.render('pages/account', {title: "Account Page"});
@@ -38,7 +34,10 @@ app.get('/admin', (req, res)=> {
 app.get('/admin/add-product', (req, res)=> {
   res.render('pages/add-product', {title: "Add product page"});
 });
-
+app.get('/product/:productId', (req, res) => {
+  const productId = req.params.productId;
+  res.render('pages/product', { title: "Product Page", productId: productId });
+});
 
 // Start the server
 app.listen(port, () => {
